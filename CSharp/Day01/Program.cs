4ac747e46efc8cdbe9cf3c2fb1c4cc7fa6b8b4d7
Victor Cazacov <victor.cazacov@bestsecret.com>
2022-12-01 22:34:50 +0100

    Puzzle 1-1 solved

---
+++ CSharp/Day01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Int32;
+
+namespace AdventOfCode2022.Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2022, Day 1");
+            var input = ReadInput("input.txt");
+            Console.WriteLine(input.Max());
+        }
+
+        private static List<int> ReadInput(string fileName)
+        {
+            var lines = System.IO.File.ReadAllLines(fileName);
+
+            var result = new List<int>();
+
+            int currentElv = 0;
+            foreach (var line in lines)
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    result.Add(currentElv);
+                    currentElv = 0;
+                }
+                else
+                {
+                    currentElv += Parse(line);
+                }
+            }
+            result.Add(currentElv);
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AdventOfCode2022
-Advent of code puzzles
+
+Advent of code 2022 puzzles
+https://adventofcode.com/
+
+- Day 1: Calorie Counting
