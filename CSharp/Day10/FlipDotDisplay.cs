0f4780c050fbf7ea7084b60593d369b1c67ab9a0
Victor Cazacov <cazacov.git@gmail.com>
2022-12-10 09:14:04 +0100

    Use flip-dot display

---
+++ CSharp/Day10/FlipDotDisplay.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Threading;
+
+namespace Day10
+{
+    /// <summary>
+    /// Control Flip-Dot display over REST API
+    /// https://github.com/cazacov/FlipDot/tree/master/webBoard
+    /// </summary>
+    class FlipDotDisplay
+    {
+        private const string BaseUrl = "http://192.168.178.61/";
+        private const int DisplayWidth = 140;
+        private const int DisplayHeight = 19;
+
+        private const int InputWidth = 40;
+        private const int InputHeight = 6;
+
+
+        public async Task Init()
+        {
+            using var client = CreateClient();
+
+            var request = new
+            {
+                line1 = "     Advent of Code 2022",
+                line2 = "  Day 10: Cathode-Ray Tube"
+            };
+            var content = new StringContent(
+                Newtonsoft.Json.JsonConvert.SerializeObject(request),
+                Encoding.UTF8, "application/json"
+            );
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var result = await client.PostAsync("textSmall", content);
+        }
+
+        public async Task ShowPuzzle1(int strength)
+        {
+            using var client = CreateClient();
+
+            var request = new
+            {
+                line1 = "                Puzzle 1",
+                line2 = $"   Signal strength: {strength}"
+            };
+            var content = new StringContent(
+                Newtonsoft.Json.JsonConvert.SerializeObject(request),
+                Encoding.UTF8, "application/json"
+            );
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var result = await client.PostAsync("textSmall", content);
+        }
+
+        public async Task ShowPuzzle2(bool [,] pixels)
+        {
+            using var client = CreateClient();
+
+            var request = new
+            {
+                text = "       Puzzle 2",
+            };
+            var content = new StringContent(
+                Newtonsoft.Json.JsonConvert.SerializeObject(request),
+                Encoding.UTF8, "application/json"
+            );
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var result = await client.PostAsync("textBig", content);
+
+            Thread.Sleep(3);
+
+            for (var y = 0; y < InputHeight; y++)
+            {
+                for (var x = 0; x < InputWidth; x++)
+                {
+                    var buffer = new bool[DisplayHeight, DisplayWidth];
+                    var yy = y * 3 + 1;
+                    var xx = 10 + x * 3 + 1;
+                    if (y != InputHeight - 1 || x != InputWidth - 1)
+                    {
+                        HLine(yy, ref buffer);
+                        VLine(xx, ref buffer);
+                    }
+
+                    for (var k = 0; k <= y; k++)
+                    {
+                        for (var l = 0; l <= ((k == y) ? x : InputWidth - 1); l++)
+                        {
+                            if (pixels[k, l])
+                            {
+                                for (var m = -1; m <= 1; m++)
+                                {
+                                    for (var n = -1; n <= 1; n++)
+                                    {
+                                        buffer[k * 3 + 1 + m, 10 + l * 3 + 1 + n] = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    await ShowFrame(buffer);
+                    Thread.Sleep(20);
+                }
+            }
+
+        }
+
+        private async Task ShowFrame(bool[,] buffer)
+        {
+            var bytesInRow = (DisplayWidth + 7) / 8;    // 8 bits per pixel, rounded up
+            var bits = new byte[DisplayHeight * bytesInRow];
+
+            for (var y = 0; y < DisplayHeight; y++)
+            {
+                for (var x = 0; x < DisplayWidth; x++)
+                {
+                    if (buffer[y, x])
+                    {
+                        bits[bytesInRow * y + x / 8] |= (byte) (0x80 >> (x & 7));
+                    }
+                }
+            }
+            
+            using var client = CreateClient();
+            var request = new
+            {
+                frameBuffer = Convert.ToBase64String(bits),
+            };
+            var content = new StringContent(
+                Newtonsoft.Json.JsonConvert.SerializeObject(request),
+                Encoding.UTF8, "application/json"
+            );
+            content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var result = await client.PostAsync("data", content);
+        }
+
+        private void HLine(int y, ref bool[,] buffer)
+        {
+            for (var i = 0; i < DisplayWidth; i++)
+            {
+                buffer[y, i] = true;
+            }
+        }
+
+        private void VLine(int x, ref bool[,] buffer)
+        {
+            for (var i = 0; i < DisplayHeight; i++)
+            {
+                buffer[i, x] = true;
+            }
+        }
+
+        private HttpClient CreateClient()
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+            return client;
+        }
+    }
+}
--- CSharp/Day10/Program.cs
+++ CSharp/Day10/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Day10
 {
     class Program
     {
-        static void Main(string[] args)
+        private static FlipDotDisplay display;
+
+        static async Task Main(string[] args)
         {
+            display = new FlipDotDisplay();
+            await display.Init();
+            Thread.Sleep(3000);
+
             Console.WriteLine("Advent of Code 2022, Day 10");
 
             var input = ReadInput("input.txt");
@@ -30,37 +36,10 @@ namespace Day10
                 }
             }
 
-            Puzzle1(regX);
-            Puzzle2(regX);
+            await Puzzle1(regX);
+            await Puzzle2(regX);
         }
-
-        private static void Puzzle2(List<int> regX)
-        {
-            Console.WriteLine("Puzzle 2:");
-            for (int y = 0; y < 6; y++)
-            {
-                for (int x = 0; x < 40; x++)
-                {
-                    var index = x + y * 40;
-                    if (Math.Abs(regX[index] - (x)) <= 1)
-                    {
-                        Console.Write(" ");
-                        var cl = Console.ForegroundColor;
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("#");
-                        Console.ForegroundColor = cl;
-                        Console.Write(" ");
-                    }
-                    else
-                    {
-                        Console.Write(" . ");
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static void Puzzle1(List<int> regX)
+        private static async Task Puzzle1(List<int> regX)
         {
             var strength = 0;
             var offset = 20;
@@ -72,6 +51,39 @@ namespace Day10
             }
 
             Console.WriteLine($"Puzzle 1: {strength}");
+            await display.ShowPuzzle1(strength);
+            Thread.Sleep(3000);
+        }
+
+        private static async Task Puzzle2(List<int> regX)
+        {
+            var result = new bool[6, 40];
+
+            Console.WriteLine("Puzzle 2:");
+            for (var y = 0; y < 6; y++)
+            {
+                for (var x = 0; x < 40; x++)
+                {
+                    var index = x + y * 40;
+                    if (Math.Abs(regX[index] - (x)) <= 1)
+                    {
+                        Console.Write(" ");
+                        var cl = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("#");
+                        Console.ForegroundColor = cl;
+                        Console.Write(" ");
+                        result[y, x] = true;
+                    }
+                    else
+                    {
+                        Console.Write(" . ");
+                        result[y, x] = false;
+                    }
+                }
+                Console.WriteLine();
+            }
+            await display.ShowPuzzle2(result);
         }
 
         private static List<Instruction> ReadInput(string fileName)
