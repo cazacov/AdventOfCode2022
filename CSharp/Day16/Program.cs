71a56c4e6e5b6b44123994a3953a5c83eb0495c6
Victor Cazacov <cazacov.git@gmail.com>
2022-12-16 21:46:03 +0100

    Refactoring

--- CSharp/Day16/Program.cs
+++ CSharp/Day16/Program.cs
@@ -12,29 +12,49 @@ namespace Day16
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2022, day 16");
+            
             var valves = ReadInput("input.txt");
 
+            // Some preparation work to speedup calculations
+            CalculatePathMap(valves);
+            
+            // Solve puzzles
             Puzzle1(valves);
             Puzzle2(valves);
+        }
+
+        private static void Puzzle1(Dictionary<string, Valve> valves)
+        {
+
+            var interesting = valves.Select(p => p.Value).Where(v => v.FlowRate > 0).ToList();
+
+            var bestScore = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            var current = valves["AA"];
+            FindOrders(current, interesting, 30, 0, ExitMaskFor(interesting), 0, ref bestScore);
+            sw.Stop();
+
+            Console.WriteLine($"Puzzle 1: {bestScore}, calculated in {sw.ElapsedMilliseconds / 1000.0} seconds");
         }
 
         private static void Puzzle2(Dictionary<string, Valve> valves)
         {
             var interesting = valves.Select(p => p.Value).Where(v => v.FlowRate > 0).ToList();
-            var n = interesting.Count;
+            var n = interesting.Count;  
 
             var maxSum = 0;
             var current = valves["AA"];
 
-
-
-            for (var i = 0; i < (1 << n); i++)
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var i = 0; i < (1 << (n - 1)); i++)  // n-1 because we need only to check half of variants. See comment below
             {
 
                 var listOne = new List<Valve>();
                 var listTwo = new List<Valve>();
 
-                for (var j = 0; j < n; j++)
+                for (var j = 0; j < n - 1; j++)
                 {
                     if ((i & (1 << j)) != 0)
                     {
@@ -45,13 +65,16 @@ namespace Day16
                         listTwo.Add(interesting[j]);
                     }
                 }
+                // Because me and the elephant are exactly same quick in problem solving the game is symmetric
+                // To half the number of considered variants we can assume it's me who visits the last interesting node.
+                listOne.Add(interesting.Last()); 
 
                 var bestScoreOne = 0;
                 var bestScoreTwo = 0;
-                FindOrders(current, listOne, 26, new List<string>() { }, valves, 0, ref bestScoreOne);
-                FindOrders(current, listTwo, 26, new List<string>() { }, valves, 0, ref bestScoreTwo);
+                FindOrders(current, listOne, 26, 0, ExitMaskFor(listOne), 0, ref bestScoreOne);
+                FindOrders(current, listTwo, 26, 0, ExitMaskFor(listTwo), 0, ref bestScoreTwo);
 
-                if (i % 1000 == 0)
+                if (i > 0 &&  i % 1000 == 0)
                 {
                     Console.WriteLine(i);
                 }
@@ -59,82 +82,87 @@ namespace Day16
                 var sum = bestScoreOne + bestScoreTwo;
                 if (sum > maxSum)
                 {
-                    Console.WriteLine(sum);
                     maxSum = sum;
+                    Console.WriteLine($"\t{maxSum}");
                 }
             }
-            Console.WriteLine($"Puzzle 2: {maxSum}");
-        }
-
-        private static void Puzzle1(Dictionary<string, Valve> valves)
-        {
-
-            var current = valves["AA"];
-            var interesting = valves.Select(p => p.Value).Where(v => v.FlowRate > 0).ToList();
-
-            for (int i = 0; i < interesting.Count; i++)
-            {
-                interesting[i].BitMask = 1 << (i);
-            }
-
-            var bestOrder = new List<string>();
-            var bestScore = 0;
-            var sw = new Stopwatch();
-            sw.Start();
-            FindOrders(current, interesting, 30, new List<string>() {}, valves, 0, ref bestScore);
             sw.Stop();
-            var ell = sw.ElapsedMilliseconds;
 
-
-            Console.WriteLine($"Puzzle 1: {bestScore}");
+            Console.WriteLine($"Puzzle 1: {maxSum}, calculated in {sw.ElapsedMilliseconds / 1000.0} seconds");
         }
 
 
-        private static void FindOrders(Valve current, List<Valve> interesting, int time, List<string> currentPath,
-            Dictionary<string, Valve> valves,
+        /// <summary>
+        /// Find an order to visit interesting valves to get best score
+        /// </summary>
+        /// <param name="current">Starting valves</param>
+        /// <param name="interesting">Valves to visit</param>
+        /// <param name="time">Remaining time</param>
+        /// <param name="currentPath">Bit mask of already visited valves</param>
+        /// <param name="exitMask">Exit mask - one with all bits set</param>
+        /// <param name="score">Score so far</param>
+        /// <param name="bestScore">Output parameter</param>
+        private static void FindOrders(Valve current, List<Valve> interesting, int time, int currentPath, int exitMask,
             int score,
             ref int bestScore)
         {
             foreach (var node in interesting)
             {
 
-                if (currentPath.Contains(node.Name))
+                if ((currentPath & node.BitMask) != 0)
                 {
                     continue;
                 }
 
-                var path = FindPath(current.Name, node.Name, valves);
+                var pathLength = pathMap[current.Name][node.Name];
 
-                var newTime = time - path.Count - 1;
+                var newTime = time - pathLength - 1;
                 var newScore = score + newTime * node.FlowRate;
 
                 if (newTime > 0)
                 {
-                    currentPath.Add(node.Name);
+                    currentPath |= node.BitMask;
                     if (newScore > bestScore)
                     {
                         bestScore = newScore;
                     }
-                    if (currentPath.Count < interesting.Count)
+                    if (currentPath != exitMask)
                     {
-                        FindOrders(node, interesting, newTime, currentPath, valves, newScore, ref bestScore);
+                        FindOrders(node, interesting, newTime, currentPath, exitMask, newScore, ref bestScore);
                     }
-                    currentPath.RemoveAt(currentPath.Count - 1);
+                    currentPath &= ~node.BitMask;
                 }
             }
         }
 
+        private static Dictionary<string, Dictionary<string, int>> pathMap;
 
-        private static Dictionary<string, List<string>> cache = new Dictionary<string, List<string>>();
+        /// <summary>
+        /// Pre-calculate cost to move from node A to B and sore it in a static dictionary pathMap
+        /// </summary>
+        /// <param name="valves"></param>
+        private static void CalculatePathMap(Dictionary<string, Valve> valves)
+        {
+            pathMap = new Dictionary<string, Dictionary<string, int>>();
+            foreach (var valve in valves.Values)
+            {
+                pathMap[valve.Name] = new Dictionary<string, int>();
+                foreach (var another in valves.Values)
+                {
+                    pathMap[valve.Name][another.Name] = FindPath(valve.Name, another.Name, valves).Count;
+                }
+            }
+        }
 
+        /// <summary>
+        /// Finds the shortest path between two valves
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="valves"></param>
+        /// <returns></returns>
         private static List<string> FindPath(string from, string to, Dictionary<string, Valve> valves)
         {
-            var cacheKey = $"{from}-{to}";
-            if (cache.ContainsKey(cacheKey))
-            {
-                return cache[cacheKey];
-            }
-
             var current = valves[from];
             var unvisited = new HashSet<string>();
             var distances = new Dictionary<string, int>();
@@ -176,22 +204,8 @@ namespace Day16
                 result.Insert(0, current.Name);
                 current = current.LeadsToValves.OrderBy(x => distances[x.Name]).First();
             }
-
-            var r1 = new List<string>();
-            r1.AddRange(result);
-            cache[cacheKey] = r1;
-
-            result.Reverse();
-            var r2 = new List<string>();
-            r2.AddRange(result);
-            cache[$"{to}-{from}"] = r2;
-
             return result;
         }
-
-
-
-
 
         private static Dictionary<string, Valve> ReadInput(string fileName)
         {
@@ -218,6 +232,24 @@ namespace Day16
                 valve.LeadsToValves = valve.LeadsToNames.Select(v => dict[v]).ToList();
             }
             return dict;
+        }
+
+        /// <summary>
+        /// Pre-calculate bit masks (to speedup checking if valve was already opened)
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private static int ExitMaskFor(List<Valve> list)
+        {
+            var allBits = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var mask = 1 << i;
+                list[i].BitMask = mask;
+                allBits |= mask;
+            }
+            //
+            return allBits;
         }
     }
 }
