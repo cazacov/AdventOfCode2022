1675bcd6b9a57d28118be66f5cdb8fa2834d64dc
Victor Cazacov <cazacov.git@gmail.com>
2022-12-20 07:08:11 +0100

    Day 20, puzzle 2 solved

--- CSharp/Day20/Program.cs
+++ CSharp/Day20/Program.cs
@@ -13,64 +13,69 @@ namespace Day20
             Console.WriteLine("Advent of Code 2022, day 20");
 
             var encrypted = ReadInput("input.txt");
-            var d = encrypted.Distinct().Count();
 
-            Puzzle1(encrypted);
-        }
-
-        private static void Puzzle1(List<int> encrypted)
-        {
             var numbers = new List<Num>();
-
-            Console.WriteLine(-2 % 7);
-            Console.WriteLine(-8 % 7);
-
-            for (int i = 0; i < encrypted.Count; i++)
+            for (var i = 0; i < encrypted.Count; i++)
             {
                 numbers.Add(new Num(i, encrypted[i]));
             }
+            Console.WriteLine($"Puzzle 1: {Puzzle(numbers, 1)}");
 
-            for (int i = 0; i < encrypted.Count; i++)
+            var encryptionKey = 811589153L;
+            numbers.Clear();
+            for (var i = 0; i < encrypted.Count; i++)
             {
-                var index = numbers.FindIndex(x => x.Position == i);
-                var elem = numbers[index];
+                numbers.Add(new Num(i, encryptionKey * encrypted[i]));
+            }
+            Console.WriteLine($"Puzzle 2: {Puzzle(numbers, 10)}");
+        }
 
-                if (elem.Value > 0)
+        private static long Puzzle(List<Num> numbers, int iterations)
+        {
+            var n = numbers.Count;
+
+            for (var j = 0; j < iterations; j++)
+            {
+                for (var i = 0; i < n; i++)
                 {
-                    numbers.RemoveAt(index);
-                    var newIndex = index + (elem.Value % (encrypted.Count - 1));
-                    if (newIndex >= encrypted.Count)
+                    var index = numbers.FindIndex(x => x.Position == i);
+                    var elem = numbers[index];
+
+                    if (elem.Value > 0)
                     {
-                        newIndex = newIndex - encrypted.Count + 1;
+                        numbers.RemoveAt(index);
+                        var newIndex = index + (elem.Value % (n - 1));
+                        if (newIndex >= n)
+                        {
+                            newIndex = newIndex - n + 1;
+                        }
+                        numbers.Insert((int)newIndex, elem);
                     }
-                    numbers.Insert(newIndex, elem);
-                }
-                else if (elem.Value < 0)
-                {
-                    numbers.RemoveAt(index);
-                    var newIndex = index + (elem.Value % (encrypted.Count - 1));
-                    if (newIndex <= 0)
+                    else if (elem.Value < 0)
                     {
-                        newIndex = newIndex + encrypted.Count - 1;
+                        numbers.RemoveAt(index);
+                        var newIndex = index + (elem.Value % (n - 1));
+                        if (newIndex <= 0)
+                        {
+                            newIndex = newIndex + n - 1;
+                        }
+                        numbers.Insert((int)newIndex, elem);
                     }
-                    numbers.Insert(newIndex, elem);
                 }
-                //Console.WriteLine(String.Join(", ", numbers.Select(x => x.Value.ToString())));
             }
 
-            var zeroIndex = numbers.FindIndex(n => n.Value == 0);
-            var i1000 = (zeroIndex + 1000) % encrypted.Count;
-            var i2000 = (zeroIndex + 2000) % encrypted.Count;
-            var i3000 = (zeroIndex + 3000) % encrypted.Count;
+            var zeroIndex = numbers.FindIndex(x => x.Value == 0);
+            var i1000 = (zeroIndex + 1000) % n;
+            var i2000 = (zeroIndex + 2000) % n;
+            var i3000 = (zeroIndex + 3000) % n;
 
-            Console.WriteLine($"Puzzle 1: {numbers[i1000].Value + numbers[i2000].Value + numbers[i3000].Value}");
-
+            return numbers[i1000].Value + numbers[i2000].Value + numbers[i3000].Value;
         }
 
         private static List<int> ReadInput(string fileName)
         {
             var lines = File.ReadAllLines(fileName);
-            return lines.Select(x => Int32.Parse(x)).ToList();
+            return lines.Select(int.Parse).ToList();
         }
     }
 
@@ -78,31 +83,12 @@ namespace Day20
     class Num
     {
         public int Position;
-        public int Value;
+        public long Value;
 
-        public Num(int position, int value)
+        public Num(int position, long value)
         {
             Position = position;
             Value = value;
         }
-
-        private sealed class PositionEqualityComparer : IEqualityComparer<Num>
-        {
-            public bool Equals(Num x, Num y)
-            {
-                if (ReferenceEquals(x, y)) return true;
-                if (ReferenceEquals(x, null)) return false;
-                if (ReferenceEquals(y, null)) return false;
-                if (x.GetType() != y.GetType()) return false;
-                return x.Position == y.Position;
-            }
-
-            public int GetHashCode(Num obj)
-            {
-                return obj.Position;
-            }
-        }
-
-        public static IEqualityComparer<Num> PositionComparer { get; } = new PositionEqualityComparer();
     }
 }
