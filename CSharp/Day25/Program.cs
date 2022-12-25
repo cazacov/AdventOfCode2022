525d391bfba65dc7995c88a9d9f28644821a156f
Victor Cazacov <cazacov.git@gmail.com>
2022-12-25 08:51:48 +0100

    Refactoring

--- CSharp/Day25/Program.cs
+++ CSharp/Day25/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,68 +9,36 @@ namespace Day25
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2022, day 25");
-            var input = ReadInput("input.txt");
-            var s = input.Sum();
-            Console.WriteLine($"{Int2Snafu(s)}");
+            
+            var result = File.ReadAllLines("input.txt")
+                .Select(Snafu2Int)
+                .Sum();
+            
+            Console.WriteLine($"Puzzle 1: {Int2Snafu(result)}");
         }
 
         private static string Int2Snafu(long n)
         {
-            if (n > -3 && n < 3)
+            var result = "";
+            do
             {
-                return DigitToChar(n);
-            }
-
-            var p = 1;
-            var pHalf = Power(p) / 2;
-            while (n < -pHalf || n > pHalf)
-            {
-                p++;
-                pHalf = Power(p) / 2;
-            }
-
-            var n1 = n + pHalf;
-            var digit = n1 / Power(p - 1) - 2;
-            var rest = n - digit * Power(p - 1);
-            var result = DigitToChar(digit);
-
-            p-=2;
-            if (p > 0)
-            {
-                pHalf = Power(p) / 2;
-                while (-pHalf <= rest && rest <= pHalf)
+                var rest = n % 5;
+                result = DigitToChar(n % 5) + result;
+                n /= 5;
+                if (rest > 2)
                 {
-                    result += '0';
-                    p--;
-                    if (p == 0)
-                    {
-                        break;
-                    }
-                    pHalf = Power(p) / 2;
+                    n++;
                 }
-            }
-            return result + Int2Snafu(rest);
+            } while (n > 0);
+            return result;
         }
 
-        private static List<long> ReadInput(string fileName)
-        {
-            var lines = File.ReadAllLines(fileName);
-            return lines
-                .Select(Snafu2Int)
-                .ToList();
-        }
-
-        private static long Snafu2Int(string line)
+        private static long Snafu2Int(string snafuString)
         {
             var result = 0L;
-            var nn = 1L;
-            for (var i = 0; i < line.Length; i++)
+            foreach (var ch in snafuString)
             {
-                var ch = line[line.Length - i - 1];
-
-                var n = CharToDigit(ch);
-                result += n * nn;
-                nn *= 5;
+                result = result * 5 + CharToDigit(ch);
             }
             return result;
         }
@@ -95,30 +62,9 @@ namespace Day25
                 0 => "0",
                 1 => "1",
                 2 => "2",
-                -1 => "-",
-                -2 => "=",
+                3 => "=",
+                4 => "-",
             };
-        }
-
-        private static Dictionary<long, long> cache = new Dictionary<long, long>();
-        private static long Power(long d)
-        {
-            if (cache.ContainsKey(d))
-            {
-                return cache[d];
-            }
-
-            if (d == 0)
-            {
-                cache[0] = 1;
-                return 1L;
-            }
-            else
-            {
-                var res = 5 * Power(d - 1);
-                cache[d] = res;
-                return res;
-            }
         }
     }
 }
